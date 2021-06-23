f85ce48b129e4eaf3d132baf2c260e54e179cbf7
Chris <toovstv@gmail.com>
2021-06-22 19:59:11 -1000

    ignoreCreeps except when stuck

--- src/Creeper.ts
+++ src/Creeper.ts
@@ -9,6 +9,15 @@ export default class Creeper extends ScreepObject {
   }
 
   moveTo(target: RoomPosition): void {
-    this.creep.moveTo(target, {ignoreCreeps:true})
+    let creep = this.creep
+    let creepMemory = this.memory['creeps'][creep.name]
+    let ignore = true
+
+    if (creepMemory.previous && Game.time - creepMemory.previous.time <= 1 && creep.pos.x == creepMemory.previous.x && creep.pos.y == creepMemory.previous.y) {
+      ignore = false
+    }
+    creepMemory.previous = { time: Game.time, x: creep.pos.x, y: creep.pos.y }
+
+    creep.moveTo(target, {ignoreCreeps:ignore})
   }
 }
